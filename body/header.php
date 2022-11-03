858712a7ffbc3bba17f95ac59b09b036ad0b6e98
anonymyoyo <paulemmanuelbohole5@gmail.com>
2022-11-03 10:52:03 +0100

    first commit

---
+++ body/header.php
@@ -0,0 +1,48 @@
+<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7/font/bootstrap-icons.css">
+<header>
+    <div class="mic_dot">
+        <div class="mic"></div>
+        <div class="dot"></div>
+    </div>
+    <nav>
+        <h3 class="brand">
+            B2T Movies
+        </h3>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Tv Shows</a></li>
+            <li><a href="#">Web Series</a></li>
+            <li><a href="#">Upcomming</a></li>
+        </ul>
+        <div class="search">
+            <input type="text" placeholder="Search...">
+        </div>
+    </nav>
+<!-- section start -->
+    <section>
+        <div class="content">
+            <h1>SQUID <span> GAME</span> </h1>
+            <h3>Saison 4</h3>
+            <div class="rating">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-half"></i>
+            </div>
+            <div class="subtitle">
+                Action <span>/</span> Drama <span>/</span> Comedie <span></span>
+            </div>
+            <div class="btns">
+                <button type=""><i class="bi bi-play-fill"></i></button>
+                <button type=""><i class="bi bi-cloud-arrow-down-fill"></i></button>
+            </div>
+        </div>
+    </section>
+
+
+<!-- cards start -->
+    <div class="cards"></div>
+</header>
